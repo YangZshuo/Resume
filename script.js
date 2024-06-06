f3ec385bf326929e9a86a41dba7c4ccf95e3a239
YangZshuo <ywgcapricorn@gmail.com>
2024-06-06 13:26:07 +0800

    add download

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('downloadBtn').addEventListener('click', function() {
+        window.location.href = 'https://www.dropbox.com/scl/fi/xtej3u50ebhpnnx64p2r7/.pdf?rlkey=sei17boadrwohncu01nmj00sz&st=o2x0k522&dl=1'; 
+    });
+});
